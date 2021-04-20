59e3abdc19047ccd2d3151254dc3ea3301aed071
Hannah Park <hannahwy9800@gmail.com>
2021-04-20 00:17:48 -0700

    “initial”

---
+++ FizzBuzzTest.java
@@ -0,0 +1,14 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz id = new FizzBuzz();
+        String result = id.fizzBuzz(2);
+        System.out.println(result);
+    }
+}
+
+
+
+
+
+
+
